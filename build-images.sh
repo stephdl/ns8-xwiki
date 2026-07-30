9f56dfb7b58997f13506af7b45e2d5d687a1a43c
Stephane de Labrusse <stephdl@de-labrusse.fr>
2026-07-30 16:09:37 +0200

    build: require core >= 3.20.1

--- build-images.sh
+++ build-images.sh
@@ -39,7 +39,7 @@ buildah config --entrypoint=/ \
     --label="org.nethserver.authorizations=traefik@node:routeadm" \
     --label="org.nethserver.tcp-ports-demand=1" \
     --label="org.nethserver.rootfull=0" \
-    --label="org.nethserver.min-core=3.12.4-0" \
+    --label="org.nethserver.min-core=3.20.1" \
     --label="org.nethserver.images=docker.io/xwiki:18.5.0-mariadb-tomcat docker.io/mariadb:11.4.12" \
     "${container}"
 # Commit the image
